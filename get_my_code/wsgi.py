ad82acaca8948dacbbe406374563bbbdf3c6e9f5
Lutfinger <przlutowski@gmail.com>
2024-06-27 17:57:58 +0200

    first commit

---
+++ get_my_code/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for get_my_code project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'get_my_code.settings')
+
+application = get_wsgi_application()
